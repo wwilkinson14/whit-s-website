7e32c9b79f77cf968f66d34c6f3e75a462ab5e5e
wwilkinson414 <wwilkinson414@gmail.com>
2018-04-15 09:49:38 -0700

    changed color theme to ocean

---
+++ instructions_no_code.R
@@ -0,0 +1,12 @@
+##Notes on blogdown eco-data science 3/14/18
+
+## find theme script in the Hugo "themes" webpages, click the "homepage" button and copy the 
+## github url and paste in the following:
+## new_site(theme = "gcushen/hugo-academic")
+
+##New rmd files get added to the content folder 
+
+##check out blogging in 10 minutes from John Harmon
+
+## tclavelle.github.io hosts content for website. Uses content in public folder and a 
+## second repo that publishes the content in the first. 
